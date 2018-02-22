06fd7def090c23d83460ce7e79b72d79b7c880a8
Pierre TASSEL <pierre.tassel@outlook.fr>
2018-02-22 16:57:33 +0100

    first commit

---
+++ script.ps1
@@ -0,0 +1,26 @@
+﻿Set-ExecutionPolicy Unrestricted
+cd C:\Users\pierr\Documents\ter\
+
+Get-Content input\ia_commence\p1_j2.txt | .\bin\ter_gcc_no_option.exe > ouput\gcc\ia_commence\no_option\p1_j2.txt
+Get-Content input\ia_commence\p2_j2.txt | .\bin\ter_gcc_no_option.exe > ouput\gcc\ia_commence\no_option\p2_j2.txt
+Get-Content input\ia_commence\p3_j2.txt | .\bin\ter_gcc_no_option.exe > ouput\gcc\ia_commence\no_option\p3_j2.txt
+Get-Content input\ia_commence\p4_j2.txt | .\bin\ter_gcc_no_option.exe > ouput\gcc\ia_commence\no_option\p4_j2.txt
+Get-Content input\ia_commence\p5_j2.txt | .\bin\ter_gcc_no_option.exe > ouput\gcc\ia_commence\no_option\p5_j2.txt
+
+Get-Content input\ia_commence\p1_j2.txt | .\bin\ter_gcc_O1.exe > ouput\gcc\ia_commence\O1\p1_j2.txt
+Get-Content input\ia_commence\p2_j2.txt | .\bin\ter_gcc_O1.exe > ouput\gcc\ia_commence\O1\p2_j2.txt
+Get-Content input\ia_commence\p3_j2.txt | .\bin\ter_gcc_O1.exe > ouput\gcc\ia_commence\O1\p3_j2.txt
+Get-Content input\ia_commence\p4_j2.txt | .\bin\ter_gcc_O1.exe > ouput\gcc\ia_commence\O1\p4_j2.txt
+Get-Content input\ia_commence\p5_j2.txt | .\bin\ter_gcc_O1.exe > ouput\gcc\ia_commence\O1\p5_j2.txt
+
+Get-Content input\ia_commence\p1_j2.txt | .\bin\ter_gcc_O2.exe > ouput\gcc\ia_commence\O2\p1_j2.txt
+Get-Content input\ia_commence\p2_j2.txt | .\bin\ter_gcc_O2.exe > ouput\gcc\ia_commence\O2\p2_j2.txt
+Get-Content input\ia_commence\p3_j2.txt | .\bin\ter_gcc_O2.exe > ouput\gcc\ia_commence\O2\p3_j2.txt
+Get-Content input\ia_commence\p4_j2.txt | .\bin\ter_gcc_O2.exe > ouput\gcc\ia_commence\O2\p4_j2.txt
+Get-Content input\ia_commence\p5_j2.txt | .\bin\ter_gcc_O2.exe > ouput\gcc\ia_commence\O2\p5_j2.txt
+
+Get-Content input\ia_commence\p1_j2.txt | .\bin\ter_gcc_O3.exe > ouput\gcc\ia_commence\O3\p1_j2.txt
+Get-Content input\ia_commence\p2_j2.txt | .\bin\ter_gcc_O3.exe > ouput\gcc\ia_commence\O3\p2_j2.txt
+Get-Content input\ia_commence\p3_j2.txt | .\bin\ter_gcc_O3.exe > ouput\gcc\ia_commence\O3\p3_j2.txt
+Get-Content input\ia_commence\p4_j2.txt | .\bin\ter_gcc_O3.exe > ouput\gcc\ia_commence\O3\p4_j2.txt
+Get-Content input\ia_commence\p5_j2.txt | .\bin\ter_gcc_O3.exe > ouput\gcc\ia_commence\O3\p5_j2.txt
