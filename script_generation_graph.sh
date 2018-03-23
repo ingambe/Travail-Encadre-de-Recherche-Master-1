eda9be5b79b8ccfd054aa072267be281c9f76b8f
Pierre TASSEL <pierre.tassel@outlook.fr>
2018-03-23 23:41:36 +0100

    debug

--- script_generation_graph.sh
+++ script_generation_graph.sh
@@ -1,27 +1,7 @@
 #!/bin/sh
 
-#COMPILATION
-
-mkdir -p bin
-g++ minimax_graph.cpp -std=c++11 -o bin/gcc_no_option.out
-g++ minimax_graph.cpp -std=c++11 -O1 -o bin/gcc_O1.out
-g++ minimax_graph.cpp -std=c++11 -O2 -o bin/gcc_O2.out
-g++ minimax_graph.cpp -std=c++11 -O3 -o bin/gcc_O3.out
-
-icpc minimax_graph.cpp -std=c++11 -O0 -o bin/intel_no_option.out
-icpc minimax_graph.cpp -std=c++11 -O1 -o bin/intel_O1.out
-icpc minimax_graph.cpp -std=c++11 -O2 -o bin/intel_O2.out
-icpc minimax_graph.cpp -std=c++11 -O3 -o bin/intel_O3.out
-
-clang++ minimax_graph.cpp -std=c++11 -O0 -o bin/clang_no_option.out
-clang++ minimax_graph.cpp -std=c++11 -O1 -o bin/clang_O1.out
-clang++ minimax_graph.cpp -std=c++11 -O2 -o bin/clang_O2.out
-clang++ minimax_graph.cpp -std=c++11 -O3 -o bin/clang_O3.out
-
-echo "fin de compilation \n"
-
 #
-#CREATION DES FICHIERS
+#CREATION DES FICHIERS DE RESULTATS
 #
 mkdir -p output
 
