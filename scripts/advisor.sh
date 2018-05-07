ec6cbf9cdcee723bbadf5a09a0722c5b2cfce5d3
Pierre TASSEL <pierre.tassel@outlook.fr>
2018-05-07 10:18:36 +0200

    dependencies

--- scripts/advisor.sh
+++ scripts/advisor.sh
@@ -10,3 +10,13 @@ icpc ../sources/minimax_annotation.cpp -std=c++11 -O3 -o ../bin/advisor_annotate
 advixe-cl -collect suitability -project-dir ../intel_tools/advisor/suitability/ -- ./../bin/advisor_annotate.out < ../input/j1.txt
 
 #advixe-cl -collect dependencies -project-dir ../intel_tools/advisor/dependencies/ -- ./../bin/advisor.out < ../input/j1.txt
+
+#/opt/intel/advisor_2018.2.0.551025/bin64/advixe-cl -collect survey -project-dir /home/ingambe/ter/intel_tools/advisor/resultats --search-dir src:p=/home/ingambe/ter/sources -- /home/ingambe/ter/bin/advisor_gui.out /home/ingambe/ter/input/j1.txt
+#/opt/intel/advisor_2018.2.0.551025/bin64/advixe-cl -collect tripcounts -flop -project-dir /home/ingambe/ter/intel_tools/advisor/resultats --search-dir src:p=/home/ingambe/ter/sources -- /home/ingambe/ter/bin/advisor_gui.out /home/ingambe/ter/input/j1.txt
+
+#/opt/intel/advisor_2018.2.0.551025/bin64/advixe-cl -collect survey -project-dir /home/ingambe/ter/intel_tools/advisor/resultats --search-dir src:p=/home/ingambe/ter/sources -- /home/ingambe/ter/bin/advisor_gui.out /home/ingambe/ter/input/j1.txt
+#/opt/intel/advisor_2018.2.0.551025/bin64/advixe-cl -collect dependencies -project-dir /home/ingambe/ter/intel_tools/advisor/resultats --search-dir src:p=/home/ingambe/ter/sources -- /home/ingambe/ter/bin/advisor_gui.out /home/ingambe/ter/input/j1.txt
+#/opt/intel/advisor_2018.2.0.551025/bin64/advixe-cl -collect map -project-dir /home/ingambe/ter/intel_tools/advisor/resultats --search-dir src:p=/home/ingambe/ter/sources -- /home/ingambe/ter/bin/advisor_gui.out /home/ingambe/ter/input/j1.txt
+
+#/opt/intel/advisor_2018.2.0.551025/bin64/advixe-cl -collect suitability -project-dir /home/ingambe/ter/intel_tools/advisor/resultats --search-dir src:p=/home/ingambe/ter/sources -- /home/ingambe/ter/bin/advisor_gui.out /home/ingambe/ter/input/j1.txt
+#/opt/intel/advisor_2018.2.0.551025/bin64/advixe-cl -collect dependencies -project-dir /home/ingambe/ter/intel_tools/advisor/resultats --search-dir src:p=/home/ingambe/ter/sources -- /home/ingambe/ter/bin/advisor_gui.out /home/ingambe/ter/input/j1.txt
