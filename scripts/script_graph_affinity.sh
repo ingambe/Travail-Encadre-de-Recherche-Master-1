843f9ba7696025a3b244ca87fecd25ca9e752644
Pierre TASSEL <pierre.tassel@outlook.fr>
2018-05-18 15:51:53 +0200

    nouveau script

---
+++ scripts/script_graph_affinity.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+echo "debut creation fichiers de sortie\n"
+
+#
+#CREATION DES FICHIERS DE RESULTATS
+#
+mkdir -p ../output/
+
+mkdir -p ../output/parallel/
+
+mkdir -p ../output/parallel/affinity/
+
+#GCC
+mkdir -p ../output/parallel/affinity/intel
+
+cat /dev/null >../output/parallel/affinity/intel/scatter.txt
+cat /dev/null >../output/parallel/affinity/intel/compact.txt
+
+
+echo "fin de creation fichier de sortie \n"
+
+echo "debug generation resultat \n"
+
+echo "Scatter\n"
+export KMP_AFFINITY=scatter
+for i in  `seq 1 20`
+do
+  echo "i : $i \n"
+  cat ../input/j1.txt | ./../bin/intel_O3_naif.out >> ../output/parallel/affinity/intel/scatter.txt
+done
+
+
+export KMP_AFFINITY=scatter
+for i in  `seq 1 20`
+do
+  echo "i : $i \n"
+  cat ../input/j1.txt | ./../bin/intel_O3_naif.out >> ../output/parallel/affinity/intel/compact.txt
+done
