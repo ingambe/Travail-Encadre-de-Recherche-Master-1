d51d5ecc5820f1822d5bb2716f01b2d31cfbafab
Pierre TASSEL <pierre.tassel@outlook.fr>
2018-06-13 01:34:20 +0200

    ajout unsorted

--- sources/minimax_hashmap.cpp
+++ sources/minimax_hashmap.cpp
@@ -303,9 +303,9 @@ int decision(CaseSuivante* cs,Position* pos,int pmax){
 	return ec._Coup[imin];
 }
 
-int valeur_minimaxAB(CaseSuivante* cs, Position* pos,const int joueur, int alpha,int beta, const int pmax, const bool gagne, std::map<Board, int> hashScoresPositions);
+int valeur_minimaxAB(CaseSuivante* cs, Position* pos,const int joueur, int alpha,int beta, const int pmax, const bool gagne, std::map<Board,int> hashScoresPositions);
 
-int calculer_coup(CaseSuivante* cs, Position* pos,const int joueur, int alpha, int beta, const int pmax,const bool gagne, std::map<Board, int> hashScoresPositions){
+int calculer_coup(CaseSuivante* cs, Position* pos,const int joueur, int alpha, int beta, const int pmax,const bool gagne, std::map<Board,int> hashScoresPositions){
 	Position newPos;
 	if (joueur==0){// MAX
 		//cout << "MAX";
@@ -354,7 +354,7 @@ int calculer_coup(CaseSuivante* cs, Position* pos,const int joueur, int alpha, i
 }
 
 /// <summary> Fonction qui calcule la valeur minimax avec coupes Alpha-Beta </summary>
-int valeur_minimaxAB(CaseSuivante* cs, Position* pos,const int joueur, int alpha,int beta, const int pmax, const bool gagne, std::map<Board, int> hashScoresPositions){
+int valeur_minimaxAB(CaseSuivante* cs, Position* pos,const int joueur, int alpha,int beta, const int pmax, const bool gagne, std::map<Board,int> hashScoresPositions){
 	// Calcule la valeur de e pour le joueur J selon que e EstUnEtatMax ou pas et pmax la
 	NUM_MINIMAX++;
 	int ajoutProf=(gagne) ? pmax : 0;
@@ -378,11 +378,7 @@ int valeur_minimaxAB(CaseSuivante* cs, Position* pos,const int joueur, int alpha
 	}
 	if (pmax == 0){
 		int valuePos = evaluer(pos);
-
-		std::map<Board,int>::iterator it = hashScoresPositions.find(pos->_Cases);
-		if (it == hashScoresPositions.end() ) {
-			hashScoresPositions.insert(std::make_pair(pos->_Cases, valuePos));
-		}
+		hashScoresPositions.insert(std::make_pair(pos->_Cases, valuePos));
 		return valuePos;
 	}
 	return calculer_coup(cs,pos,joueur,alpha,beta,pmax,gagne, hashScoresPositions);
@@ -395,7 +391,7 @@ int valeur_minimaxAB(CaseSuivante* cs, Position* pos,const int joueur, int alpha
 int decisionAB(CaseSuivante* cs,Position* pos,int pmax, bool gagne){
 	// on regarde le nombre de case vide et on ajoute de la profondeur eventuellement
 	// k case vide = profmax * k/12
-	std::map<Board, int> hashScoresPositions;
+	std::map<Board,int> hashScoresPositions;
 	int k=0;
 	for(int i=0;i<6;i++){
 		if (pos->_Cases[i] == 0) k++;
--- sources/minimax_hashmap.h
+++ sources/minimax_hashmap.h
@@ -1,9 +1,42 @@
 #include <array>
+//#include <unordered_map>
 #include <map>
 ///<summary>
 ///Structure d?terminant une position de jeu 
 ///</summary>
 typedef std::array<int, 12> Board;
+
+/**
+namespace std
+{
+    template<typename T, size_t N>
+    struct hash<array<T, N> >
+{
+    typedef array<T, N> argument_type;
+    typedef size_t result_type;
+
+    std::size_t operator()(const std::array<int, 12>& a) {
+        std::size_t h = 0;
+
+        for (auto e : a) {
+            h ^= std::hash<int>{}(e)  + 0x9e3779b9 + (h << 6) + (h >> 2);
+        }
+        return h;
+    }
+
+    result_type operator()(const argument_type& a) const
+    {
+        hash<T> hasher;
+        result_type h = 0;
+        for (result_type i = 0; i < N; ++i)
+        {
+            h = h * 31 + hasher(a[i]);
+        }
+        return h;
+    }
+};
+}
+ **/
 
 struct Pos {
 	//int _Cases[2][6]; // 6 cases et 2 joueurs
